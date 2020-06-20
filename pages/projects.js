ea52433e69575c614f90ec002e4b29b6a1bafe50
Milo Gertjejansen <milo@milogert.com>
2020-06-20 02:38:00 -0500

    Added production flag to make sure we are picking up the correct url

--- pages/projects.js
+++ pages/projects.js
@@ -15,9 +15,14 @@ const Projects = ({ error, pinnedRepos }) => <>
       <p>These are personal projects I have worked on in the past. Feel free to send me an email if you have questions or suggestions!</p>
     </Hero>
 
-    { error && <p>
-      {error.message} <code>{error.code}</code>
-    </p>}
+    { error &&
+      <div className="content">
+        <div className="p-4 bg-red-500 bg-opacity-25 border-2 rounded border-red-500 text-red-700 shadow-lg">
+          {error.message}
+          <pre><code>{error.code}</code></pre>
+        </div>
+      </div>
+    }
 
     { pinnedRepos.map(({ id, ...rest }, idx) => <RepoRow key={id} {...rest} wrapRow={idx % 2 === 1} /> ) }
 
