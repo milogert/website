763143f8bda3384750993de38ddf6b012babf1be
Milo Gertjejansen <milo@milogert.com>
2023-12-17 13:36:38 -0600

    fix 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Hero } from 'components/hero'
 
 const Custom404 = () => {
   return (
-    <Hero title="Page Not Found">
+    <div className="flex flex-col gap-8">
+      <h1 className="text-5xl">404</h1>
       <p className="my-4">I couldn&apos;t find the page you are looking for.</p>
-    </Hero>
+    </div>
   )
 }
 
