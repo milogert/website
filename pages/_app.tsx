a959251e3ff125d2f6a285283d61cb506469ecf8
Milo Gertjejansen <milo@milogert.com>
2023-12-17 13:46:49 -0600

    fix fa bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,15 @@ import React from 'react'
 import { Layout } from 'components/layout'
 import { Nav } from 'components/nav'
 
+// Copied from https://stackoverflow.com/a/59429852
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import '@fortawesome/fontawesome-svg-core/styles.css'
+// Prevent fontawesome from adding its CSS since we did it manually above:
+// eslint-disable-next-line import/order
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false; /* eslint-disable import/first */
+
 const App = ({ Component, pageProps }) => {
   return (
     <Layout>
